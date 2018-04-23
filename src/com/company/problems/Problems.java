0b5ca2b4f7685ff5ad7baec496a1d5f6984a2d91
Artem Krykun <akrykun@playtika.com>
2018-04-23 18:10:22 +0300

    clean

---
+++ src/com/company/problems/Problems.java
@@ -0,0 +1,9 @@
+package com.company.problems;
+
+public class Problems {
+
+    public static void main(String[] args) {
+        //YOUR code
+    }
+
+}
--- src/com/company/problems/problems.java
+++
@@ -1,9 +0,0 @@
-package com.company.problems;
-
-public class problems {
-
-    public static void main(String[] args) {
-        //YOUR code
-    }
-
-}
