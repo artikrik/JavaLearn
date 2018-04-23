1ae194939dca20d1836409eb6e47a1018ca9ac47
Artem Krykun <artikrik>
2018-04-24 00:54:17 +0300

    solved 2-4

---
+++ src/com/company/problems2/Problems2_4.java
@@ -0,0 +1,49 @@
+package com.company.problems2;
+
+/*
+Создайте 2 массива из 5 случайных целых чисел из отрезка [0;5] каждый, выведите массивы на экран в двух отдельных строках. Посчитайте среднее арифметическое элементов каждого массива и сообщите, для какого из массивов это значение оказалось больше (либо сообщите, что их средние арифметические равны).*/
+
+import java.util.Random;
+
+public class Problems2_4 {
+    private static final int[] FIRST_ARRAY = new int[5];
+    private static final int[] SECOND_ARRAY = new int[5];
+
+    private static int calcAverageNum(int[] a) {
+        int sum = 0;
+        for (int elA :
+                a) {
+            sum = elA + sum;
+        }
+        return sum/a.length;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < FIRST_ARRAY.length; i++) {
+            Random random = new Random();
+            int num = 1 + random.nextInt(10);
+            FIRST_ARRAY[i] = num;
+            System.out.print("  " + FIRST_ARRAY[i] + "  ");
+        }
+        System.out.print("\n");
+
+        for (int i = 0; i < SECOND_ARRAY.length; i++) {
+            Random random = new Random();
+            int num = 1 + random.nextInt(10);
+            SECOND_ARRAY[i] = num;
+            System.out.print("  " + SECOND_ARRAY[i] + "  ");
+        }
+        System.out.println("\n");
+        System.out.println("FIRST_ARRAY " + calcAverageNum(FIRST_ARRAY));
+        System.out.println("SECOND_ARRAY " + calcAverageNum(SECOND_ARRAY));
+        System.out.print("\n");
+
+        if (calcAverageNum(FIRST_ARRAY) > calcAverageNum(SECOND_ARRAY)) {
+            System.out.println("FIRST_ARRAY " + calcAverageNum(FIRST_ARRAY));
+        } else if (calcAverageNum(FIRST_ARRAY) < calcAverageNum(SECOND_ARRAY)) {
+            System.out.println("SECOND_ARRAY " + calcAverageNum(SECOND_ARRAY));
+        } else {
+            System.out.println("Numbers are equals");
+        }
+    }
+}
