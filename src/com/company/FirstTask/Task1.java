4f01102969d9c6f66327f4c816c5343be30c420f
oginda <oginda@playtika.com>
2018-04-25 16:31:53 +0300

    почистил =)

---
+++ src/com/company/FirstTask/Task1.java
@@ -0,0 +1,7 @@
+package com.company.FirstTask;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("dsgs");
+    }
+}
--- src/com/company/w2/w2_3.java
+++
@@ -1,7 +0,0 @@
-package com.company.w2;
-
-public class w2_3 {
-    public static void main(String[] args) {
-        System.out.println("WORKS");
-    }
-}
