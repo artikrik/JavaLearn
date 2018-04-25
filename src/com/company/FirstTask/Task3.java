d5a8b51ee193c81320e3994328937b11b9e42c0e
oginda <oginda@playtika.com>
2018-04-25 16:45:14 +0300

    добавил 4 варианта реализации первой задачи

---
+++ src/com/company/FirstTask/Task3.java
@@ -0,0 +1,18 @@
+package com.company.FirstTask;
+
+//третий вариант
+
+public class Task3 {
+    public static void main(String[] args) {
+        int a = 1234;
+        if (a < 10) {
+            System.out.println("1");
+        } else if (a < 100) {
+            System.out.println("2");
+        } else if (a < 1000) {
+            System.out.println("3");
+        } else if (a < 10000) {
+            System.out.println("4");
+        }
+    }
+}
---
+++ src/com/company/FirstTask/Task4.java
@@ -0,0 +1,10 @@
+package com.company.FirstTask;
+
+//четвёртый вариант
+
+public class Task4 {
+    public static void main(String[] args) {
+        int a = 1234567;
+        System.out.println("Символов: " + String.valueOf(a).length());
+    }
+}
