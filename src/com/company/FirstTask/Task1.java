f24c0a7f95f3af63dcc018ac8ecf9004cf456138
oginda <oginda@playtika.com>
2018-04-25 16:42:56 +0300

    добавил 4 варианта реализации первой задачи

--- src/com/company/FirstTask/Task1.java
+++ src/com/company/FirstTask/Task1.java
@@ -1,7 +1,15 @@
 package com.company.FirstTask;
 
+//программа для подсчёта количества символов в цифре
+//первый вариант
+
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("dsgs");
+
+        int a = 123456790;
+        int b = 0;
+        for( ; a!=0; a/=10)
+            ++b;
+        System.out.println(b);
     }
 }
