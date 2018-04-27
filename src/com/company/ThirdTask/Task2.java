6446fe7df66efa636b15335161a46db60dd84b81
oginda <oginda@playtika.com>
2018-04-27 14:47:07 +0300

    Создайте массив из 15 случайных целых чисел из отрезка [0;9]. Выведите массив на экран. Подсчитайте сколько в массиве чётных элементов и выведете это количество на экран на отдельной строке.

---
+++ src/com/company/ThirdTask/Task2.java
@@ -0,0 +1,25 @@
+package com.company.ThirdTask;
+
+//Создайте массив из 15 случайных целых чисел из отрезка [0;9]. Выведите
+//массив на экран. Подсчитайте сколько в массиве чётных элементов и
+//выведете это количество на экран на отдельной строке.
+
+import java.util.Random;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        int[] a = new int[15];
+        int b = 0;
+        Random random = new Random();
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = random.nextInt(9);
+            System.out.print(a[i] + " ");
+                if (a[i] > 0 & a[i] % 2 == 0)
+                b++;
+        }
+        System.out.println();
+        System.out.println("Всего чётных чисел: " + b);
+    }
+}
