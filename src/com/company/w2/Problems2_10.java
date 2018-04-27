c5a5483fbfb786aefaf7d2a1aced325d611f4d22
Art Krik <artikrik@gmail.com>
2018-04-28 00:10:35 +0300

    temp 2-10

--- src/com/company/w2/Problems2_10.java
+++ src/com/company/w2/Problems2_10.java
@@ -1,10 +1,22 @@
 package com.company.w2;
 
+import java.util.Random;
+
 /*
 В массиве А размером 10х5 определить самые большие значения в каждой строке и среди них – наименьшее знаение.
 */
 public class Problems2_10 {
-    public static void main(String[] args) {
+    private static final int[][] A = new int[10][5];
 
+    public static void main(String[] args) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                Random random = new Random();
+                int num = random.nextInt(10);
+                A[i][j] = num;
+                System.out.print(A[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
