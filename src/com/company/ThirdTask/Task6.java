d3d1ff9e057fb3aad1ba58673c55501375ce27fb
oginda <oginda@playtika.com>
2018-04-28 21:21:54 +0300

    Создайте массив из 12 случайных целых чисел из отрезка [-15;15]. Определите какой элемент является в этом массиве максимальным и сообщите индекс его последнего вхождения в массив.

--- src/com/company/ThirdTask/Task6.java
+++ src/com/company/ThirdTask/Task6.java
@@ -10,19 +10,19 @@ public class Task6 {
     public static void main(String[] args) {
         int a[] = new int[12];
         int max = 0, indexMax = 0;
-        Random random = new Random();
 
-        for (int i = 0; i < a.length; i++){
+        for (int i = 0; i < 12; i++) {
             a[i] = (int) (Math.round(Math.random() * 30 - 15));
             System.out.print(a[i] + " ");
 
-            if (i == 0){
+            if (i == 0) {
                 max = a[i];
                 indexMax = i;
             }
-            if (i >= 1 && a[i] > max)
+            if (i >= 1 && a[i] > max) {
                 max = a[i];
                 indexMax = i;
+            }
         }
         System.out.println();
         System.out.println("Максимальный элемент массива: " + max + ", а его индекс: " + indexMax);
