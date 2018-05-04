43f1490bf7479a34bfbcfb640d92780df3c8d04c
Alexandr Dolgyi <veterokvn.ua@gmail.com>
2018-05-04 15:18:19 +0300

    add w3

--- src/com/company/w2/Task4.java
+++ src/com/company/w2/Task4.java
@@ -1,47 +1,39 @@
 package com.company.w2;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Task4 {
     public static void main(String[] args) {
-        int[] massFirst = new int[5];
-        int[] massSecond = new int[5];
+            int[] massFirst = new int[5];
+            int[] massSecond = new int[5];
 
-        Random random = new Random(8);
-        for (int i = 0; i < massFirst.length; i++) {
-            massFirst[i] = random.nextInt(5)+1;
-        }
-        for (int i = 0; i < massSecond.length; i++) {
-            massSecond[i] = random.nextInt(5)+1;
-        }
-        // Выводим и находим Среднее Арифметичное масивов
-        out(massFirst);
-        out(massSecond);
-        arithmeticAverage(massFirst, massSecond);
-    }
+            Random random = new Random(3);
+            Random random2 = new Random(1);
+            for (int i = 0; i < massFirst.length; i++) {
+                massFirst[i] = random.nextInt(5) + 1;
+            }
+            for (int i = 0; i < massSecond.length; i++) {
+                massSecond[i] = random2.nextInt(5) + 1;
+            }
+            // Выводим и находим Среднее Арифметичное масивов
+            System.out.println(Arrays.toString(massFirst));
+            System.out.println(Arrays.toString(massSecond));
 
-    public static void out(int[] a) {
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i] + "; ");
+            if (arithmeticAverage(massFirst) > arithmeticAverage(massSecond))
+                System.out.println("Среднее арифметическое элементов  1 массива больше");
+            else if (arithmeticAverage(massFirst) < arithmeticAverage(massSecond))
+                System.out.println("Среднее арифметическое элементов 2 массива больше");
+            else System.out.println("Среднее арифметическое элементов каждого массива равны");
         }
-        System.out.println();
-    }
 
-    public static void arithmeticAverage(int[] a, int[] b) {
+    public static int arithmeticAverage(int[] a) {
         int sum = 0;
-        int sum2 = 0;
         for (int i = 0; i < a.length; i++) {
-            sum +=a[i];
+            sum += a[i];
         }
-        for (int i = 0; i < b.length; i++) {
-            sum2 += b[i];
-        }
-        sum/=a.length;
-        sum2/=b.length;
-
-        if (sum > sum2) System.out.println("Среднее арифметическое элементов  1 массива больше");
-        else if (sum < sum2) System.out.println("Среднее арифметическое элементов 2 массива больше");
-        else System.out.println("Среднее арифметическое элементов каждого массива равны");
+        sum /= a.length;
+        return sum;
     }
 
 }
