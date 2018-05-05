ef0bec1537a5e5e92eee592a92383828324fb79d
odolgyi <veterokvn.ua@gmail.com>
2018-05-05 23:37:13 +0300

    //binary search String + overloadLinearSearch

--- src/com/company/w3/BinarySearchNum.java
+++ src/com/company/w3/BinarySearchNum.java
@@ -5,9 +5,9 @@ import java.util.Random;
 
 public class BinarySearchNum {
     public static void main(String[] args) {
-        int num = 494; // переменная для поиска числа
+        int num =8; // переменная для поиска числа
 
-        int[] mass = new int[100];
+        int[] mass = new int[12];
         Random random = new Random(50);
         for (int i = 0; i < mass.length; i++) {
             mass[i] = random.nextInt(500);
@@ -15,23 +15,23 @@ public class BinarySearchNum {
         System.out.println("Searching element has index " + binarySearch(mass, num) + " in the array");
     }
 
-    static int binarySearch(int[] mass, int num) {
+    static int binarySearch(int[] mass, int num)
+
+    {
         Arrays.sort(mass);
         System.out.println(Arrays.toString(mass));
         int begin = 0;
         int end = mass.length - 1;
         int middle = (mass.length - 1) / 2;
-        int i = 0;
-        while (num == mass[i] || begin < end - 1) {
+        while (  begin <= end ) {
+            middle = begin+(end-begin)/2;
             if (num == mass[middle]) return middle;
-            else if (num > mass[middle]) {
-                begin = middle;
-                middle = (end + middle) / 2;
+             if (num > mass[middle]) {
+                begin = middle+1;
             } else {
                 end = middle;
-                middle = (end - middle) / 2;
             }
         }
-        return end;
+        return middle;
     }
 }
