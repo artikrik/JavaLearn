819dff619a902c991a0e053ef4438b0b00e713f0
aalokhin <12104@ukr.net>
2018-05-08 16:52:48 +0300

    Линейный поиск

---
+++ src/com/company/BubbleSort.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = new int[8];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (100 * Math.random());
+            System.out.print(" " + array[i]);
+        }
+        System.out.println(" ");
+
+        for(int i=0; i<array.length; i++){
+            for(int j=0; j<array.length-1-i; j++){
+                if(array[j]>array[j+1]){
+                    int c;
+                    c=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=c;
+                }
+            }
+        }
+
+
+        for(int i=0; i<array.length; i++){
+            System.out.print(array[i]+" ");
+        }
+
+    }
+}
+
+
