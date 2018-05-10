cbc3ece1d716e367448473d906e04682edb7ec50
vlisovyi <Amalker_1983>
2018-05-10 11:36:42 +0300

    W3 liner search. +2 tasks.

--- src/com/company/w3/SortAndSearch.java
+++ src/com/company/w3/SortAndSearch.java
@@ -32,24 +32,59 @@ public class SortAndSearch {
          * Линейный поиск.
          */
 
-        Scanner s = new Scanner(System.in);
-        System.out.println("Введите число для поиска:");
-        int x = s.nextInt();
-        Random random = new Random();
-        int arrValue = 10;
-        int[] linerSearchArray = new int[arrValue];
-        int element = linerSearchArray[0];
-        for (int i = 0; i < linerSearchArray.length; i++) {
-            linerSearchArray[i] = random.nextInt(20);
-            System.out.print(linerSearchArray[i] + "\t");// создание и заполнение массива
-            if (linerSearchArray[i] == x)
-                element = linerSearchArray[i];
-        }
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Введите число для поиска:");
+//        int x = s.nextInt();
+//        Random random = new Random();
+//        int arrValue = 10;
+//        int[] linerSearchArray = new int[arrValue];
+//        int element = linerSearchArray[0];
+//        for (int i = 0; i < linerSearchArray.length; i++) {
+//            linerSearchArray[i] = random.nextInt(20);
+//            System.out.print(linerSearchArray[i] + "\t");// создание и заполнение массива
+//            if (linerSearchArray[i] == x)
+//                element = linerSearchArray[i];
+//        }
+//
+//        if (element == x)
+//            System.out.println("\nЧисло есть");
+//        else
+//            System.out.println("\nЧисла нет");
 
-        if (element == x)
-            System.out.println("\nЧисло есть");
-        else
-            System.out.println("\nЧисла нет");
+
+        /**
+         * Линейный поиск String.
+         */
+
+//
+//        char[] arr = {'a', 'd', 'f', 'l', 'o', 'z'};
+//        char charToFind = 'a';
+//        char element = arr[0];
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i]==charToFind)
+//                element = arr[i];
+//        }
+//        if (element == charToFind)
+//            System.out.println("Буква есть");
+//        else
+//            System.out.println("Буква не найдена");
+
+        /**
+         * Сортировка "пузырьком" String.
+         */
+
+        char[] arr = {'o', 'd', 'f', 'l', 'a', 'z'};
+
+        for (int i = 0; i < arr.length - 1; i++) { // число итераций прогона для каждого числа
+            for (int j = arr.length - 1; j > 0 + i; j--) { // прогон одного числа по массиву
+                if (arr[j] < arr[j - 1]) {
+                    char tmp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
 
