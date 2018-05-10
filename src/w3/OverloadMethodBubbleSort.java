aa1f969293ffbc1a370b85bf6c76c901b9dfd525
kpoberezhniuk <kpoberezhniuk@playtika.com>
2018-05-10 13:10:54 +0300

    OverloadMethodBubbleSort_fix0

--- src/w3/OverloadMethodBubbleSort.java
+++ src/w3/OverloadMethodBubbleSort.java
@@ -4,29 +4,23 @@ import java.util.Arrays;
 
 public class OverloadMethodBubbleSort {
     public static void main(String[] args) {
-        int[] massInt = new int[0];
-        String[] masString = new String[]{""};
-
-        massInt = createArray(massInt);
+        int[] massInt = new int[25];
+        for (int i = 0; i < massInt.length; i++) {
+            massInt[i] = (int) (100 * Math.random());
+        }
         System.out.println(Arrays.toString(massInt));
+
+        String[] masString = new String[]{"some", "random", "words", "needs", "to", "be", "sorted", "by", "bubble", "method", "august", "king"};
 
         massInt = bubbleMagic(massInt);
         System.out.println(Arrays.toString(massInt) + " - bubble sorted");
 
-        masString = createArray(masString);
         System.out.println("\n" + Arrays.toString(masString));
 
         masString = bubbleMagic(masString);
         System.out.println(Arrays.toString(masString) + " - bubble sorted");
     }
 
-    public static int[] createArray(int[] massInt) {
-        int[] mass0 = new int[25];
-        for (int i = 0; i < mass0.length; i++) {
-            mass0[i] = (int) (100 * Math.random());
-        }
-        return mass0;
-    }
 
     public static int[] bubbleMagic(int[] mass) {
         int tor;
@@ -43,10 +37,6 @@ public class OverloadMethodBubbleSort {
         return mass;
     }
 
-    public static String[] createArray(String[] masString) {
-        String[] array = new String[]{"some", "random", "words", "needs", "to", "be", "sorted", "by", "bubble", "method", "august", "king"};
-        return array;
-    }
 
     public static String[] bubbleMagic (String[]bubblesort){
        String tmp;
