bdd08b31ca96b0e5fe311b3cf84c336e68c65f5c
aalokhin <12104@ukr.net>
2018-05-10 16:37:09 +0300

    update

--- src/w3/LenearSearchString.java
+++ src/w3/LenearSearchString.java
@@ -4,19 +4,5 @@ import java.util.Scanner;
 public class LenearSearchString {
     public static void main(String[] args) {
 
-        String[] mas = {"море", "пляж", "риба", "чайка"};
-        Scanner find = new Scanner(System.in);
-        System.out.println("введіть слово");
-        String a = find.nextLine();
-        int i;
-        int index=-1;
-        for (i = 0; i < mas.length; ++i) {
-            if (mas[i].equals(a)) {
-                index=i;
-                System.out.println("Индекс=" +i);
-            }
-        }
-        if (index==-1)
-            System.out.println("такого слова нема");
     }
 }
