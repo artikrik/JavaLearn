e422ab5485461f216744109df1a832d16eb57a74
aalokhin <12104@ukr.net>
2018-05-10 15:44:07 +0300

    переделаное

--- src/w3/BubbleSortString.java
+++ src/w3/BubbleSortString.java
@@ -1,27 +1,28 @@
 package w3;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class BubbleSortString {
     public static void main(String[] args) {
+        Scanner letters = new Scanner(System.in);
+        String tempStr;
 
-        String[] mas = {"море", "чайка", "пісок", "пляж", "риба", "А"};
-        int i; int j;
-        int index=-1;
-        for (i = 0; i < mas.length; i++) {
-            System.out.print(mas[i] + " ");
-        }
-        System.out.println(" ");
-        System.out.println("Відсортований масив:");
-        for(i = mas.length-1 ; i > 0 ; i--){
-            for(j = 0 ; j < i ; j++){
-                if (mas[j].compareTo(mas[j+1]) > 0 ){
-                    String tmp = mas[j];
-                    mas[j] = mas[j+1];
-                    mas[j+1] = tmp;
+        System.out.print("Enter the strings > ");
+        String s1 = new String(letters.nextLine());
+
+        String[] t1 = s1.split("");
+
+        for (int t = 0; t < t1.length - 1; t++) {
+            for (int i = 0; i < t1.length - t - 1; i++) {
+                if (t1[i + 1].compareTo(t1[i]) < 0) {
+                    tempStr = t1[i];
+                    t1[i] = t1[i + 1];
+                    t1[i + 1] = tempStr;
                 }
             }
         }
-        System.out.print(Arrays.toString(mas));
+        for (int i = 0; i < t1.length; i++) {
+            System.out.print(t1[i]);
+        }
     }
 }
