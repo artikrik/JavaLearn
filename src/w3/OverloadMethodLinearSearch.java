5738b7960f954b47343864d07849da78ad5b1674
kpoberezhniuk <kpoberezhniuk@playtika.com>
2018-05-10 14:37:17 +0300

    OverloadMethodBubbleSort_fix0

--- src/w3/OverloadMethodLinearSearch.java
+++ src/w3/OverloadMethodLinearSearch.java
@@ -4,65 +4,58 @@ import java.util.Arrays;
 
 public class OverloadMethodLinearSearch {
     public static void main(String[] args) {
-        int[] randomMass = new int[0];
-        int digit = 7;
-
-        String[] str = new String[]{""};
-        String word = "beeee";
-
-        randomMass = arraysCreator(randomMass);
+        int digit = 60;
+        boolean states;
+        int[] randomMass = new int[25];
+        for (int i = 0; i < randomMass.length; i++) {
+            randomMass[i] = (int) (10 * Math.random());
+        }
         System.out.print(Arrays.toString(randomMass) + "\t");
 
-        boolean states;
-        states = linearSearch(randomMass, str, digit, word);
+
+        String word = "heree";
+        String[] str = new String[]{"some", "random", "words", "needs", "to", "be", "here"};
+        System.out.println("\n" + Arrays.toString(str));
+
+        states = linearSearch(randomMass, digit);
         if (states) {
             System.out.println("\n" + Integer.toString(digit) + " IS found ");
         } else
             System.out.println("\n" + digit + " can't be found in this array ");
 
 
-        str = createArrayForString(str);
-        System.out.println(Arrays.toString(str));
-
-        states = linearSearch(randomMass, str, digit, word);
+        states = linearSearch(str, word);
         if (states) {
             System.out.println(" word is present ");
         } else
-            System.out.println(" word can not be found. Try again ");
+            System.out.println("- " + word + " -" + " word can not be found. Try again ");
     }
 
-    public static int[] arraysCreator(int[] randomMass) {
-        int[] Mass = new int[10];
-        for (int i = 0; i < Mass.length; i++) {
-            Mass[i] = (int) (10 * Math.random());
-            randomMass = Mass;
-        }
-        return randomMass;
-    }
 
-    public static boolean linearSearch(int[] mass0, String[]str, int digit, String word) {
+    public static boolean linearSearch(int[] randomMass, int digit) {
         boolean states = true;
         int i;
-        for (i = 0; i < mass0.length; i++) {
-            if (digit == mass0[i]) {
+        for (i = 0; i < randomMass.length; i++) {
+            if (digit == randomMass[i]) {
                 states = true;
                 break;
             }
         }
-        if (i == mass0.length) {
+        if (i == randomMass.length) {
             states = false;
         }
-        for (int j = 0; j < str.length; j++) {
-            if (word.equals(str[j])) {
-                states = true;
-            }
-        }
+
         return states;
     }
 
-    public static String[] createArrayForString(String[] str) {
-        String[] str0 = new String[]{"some", "random", "words", "needs", "to", "be", "here"};
-        str = str0;
-        return str;
+    public static boolean linearSearch(String[] str, String word) {
+        boolean states = false;
+        for (int j = 0; j < str.length; j++) {
+            if (word.equals(str[j])) {
+                states = true;
+                break;
+            }
+        }
+        return states;
     }
 }
