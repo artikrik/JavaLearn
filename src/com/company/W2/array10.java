12e27960a67e5d39d30920ba5fb45e95204c2a2e
omakarchuk <alexandermakarchuk2@gmail.com>
2018-05-10 12:05:39 +0300

    fixed 2.10

--- src/com/company/W2/array10.java
+++ src/com/company/W2/array10.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class array10 {
     public static void main(String[] args) {
         Random r = new Random();
-        int[][] A = new int[10][5];
+        int[][] A = new int[3][3];
         int i;
         int j;
-        int min = 10;
+        int[] mas = new int[A.length];
+        int min;
         for (i = 0; i < A.length; i++) {
             for (j = 0; j < A[i].length; j++) {
                 A[i][j] = r.nextInt(10);
@@ -16,17 +17,21 @@ public class array10 {
             }
             System.out.println();
         }
+        mas = new int[A.length];
         for (i = 0; i < A.length; i++) {
             int max = 0;
-
-            for (j = 0; j< A[i].length; j++) {
+            for (j = 0; j < A[i].length; j++) {
                 if (A[i][j] > max) {
                     max = A[i][j];
                 }
             }
+            mas[i] = max;
             System.out.println("максимальне число в рядку=" + max);
-            if (min > max) {
-                min = max;
+        }
+        min = mas[0];
+        for (i = 0; i < mas.length; i++) {
+            if (mas[i] < min) {
+                min = mas[i];
             }
         }
         System.out.println("найменше серед максимальних чисел=" + min);
