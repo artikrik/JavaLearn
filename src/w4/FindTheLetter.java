48031cb82107b2ee1965f5f0f546c7b81e1a1442
kpoberezhniuk <kpoberezhniuk@playtika.com>
2018-05-13 15:32:10 +0300

    Merge remote-tracking branch 'origin/kpoberezhniuk' into kpoberezhniuk
    # Conflicts:
    #	src/w4/FindTheLetter.java

--- src/w4/FindTheLetter.java
+++ src/w4/FindTheLetter.java
@@ -1,21 +1,7 @@
-/*Написать программу игры в угадывание букв.
-Пользователь должен угадать загаданную программой латинскую букву вводя свой вариант буквы с клавиатуры.
-Если пользователь угадал букву,
-программа выводит сообщение о выигрыше и количестве попыток пользователя.
-Если буква не угадана, программа предлагает ввести другую букву.
-При этом программа выводит подсказки.
-Если пользователь ввел букву меньше загаданной – вывести сообщение об этом (например, «Too low!»).
-Аналогично при вводе буквы больше загаданной (например, «Too high!»).
-* Если введена не латинская буква – предложить пользователю внимательно ознакомиться с правилами игры.
-* Программа должна быть не чувствительна к регистру введенной пользователем буквы.
-Например, при загаданной букве ‘G’ ответы пользователя ‘G’ или ‘g’ в равной степени считаются правильными.
-Игра продолжается до выигрыша пользователя.
-Тексты выводимых программой сообщений – на Ваше усмотрение.
-Для загадывания букв использовать генератор псевдослучайных чисел.*/
 
 package w4;
 
-import java.util.Arrays;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -26,15 +12,11 @@ public class FindTheLetter {
         char letterNeedToBeFind = (char) ('A' + new Random().nextInt('Z' - 'A'));
         int numberOfAttempts = 0;
         boolean states;
-        /*char[] arrayChar = new char[26];
-        for (int i = 0; i < arrayChar.length; i++) {
-            arrayChar[i] = (char) ('A' + i);
-        }
-        System.out.println(Arrays.toString(arrayChar));*/
+
         System.out.println("Hidden letter - " + letterNeedToBeFind);
         System.out.println("\nTry to find the hidden letter");
 
-        for (char i = 0; i < letterNeedToBeFind;i++) {
+        for (int i = 0; ;i++) {
             char inputLetter = scan.next().charAt(0);
             inputLetter = Character.toUpperCase(inputLetter);
             states = validationChacking(inputLetter);
