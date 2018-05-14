c02321e94b57df8f41ebe49ce10c91372094703a
oginda <oginda@playtika.com>
2018-05-14 12:53:58 +0300

    refactoring

--- src/com/company/ForthTask/BinarySearchString.java
+++ src/com/company/ForthTask/BinarySearchString.java
@@ -6,33 +6,32 @@ import java.util.Scanner;
 public class BinarySearchString {
     public static void main(String[] args) {
 
-        String[] mass = {"iPhone", "Samsung", "Nokia", "Xiaomi", "Meizu",
+        String[] phoneList = {"iPhone", "Samsung", "Nokia", "Xiaomi", "Meizu",
                 "LG", "Huawei", "Asus", "Sony", "Motorola", "HTC", "1+"};
         int numberPosition = 0;
         String phone = " ";
 
-        for (int i = 0; i < mass.length; i++) {
-            Arrays.sort(mass);
+        for (int i = 0; i < phoneList.length; i++) {
+            Arrays.sort(phoneList);
         }
 
-        System.out.println(Arrays.toString(mass));
+        System.out.println(Arrays.toString(phoneList));
         System.out.println("Введите марку телефона, который ищите: ");
-
         Scanner scanner = new Scanner(System.in);
         phone = scanner.next();
 
         String massIndex = " ";
         int first = 0;
-        int last = mass.length - 1;
+        int last = phoneList.length - 1;
 
         while (first <= last) {
             numberPosition = (first + last) / 2;
-            if ((mass[numberPosition].compareTo(phone)) == 0) {
-                massIndex = mass[numberPosition];
+            if ((phoneList[numberPosition].compareTo(phone)) == 0) {
+                massIndex = phoneList[numberPosition];
                 break;
-            } else if ((mass[numberPosition].compareTo(phone)) > 0) {
+            } else if ((phoneList[numberPosition].compareTo(phone)) > 0) {
                 last = numberPosition - 1;
-            } else if ((mass[numberPosition].compareTo(phone)) < 0) {
+            } else if ((phoneList[numberPosition].compareTo(phone)) < 0) {
                 first = numberPosition + 1;
             }
         }
