8d64f1c6326febdb46807344041eeb86605cfdfc
aalokhin <12104@ukr.net>
2018-05-14 11:37:57 +0300

    Задание 3.8

---
+++ src/w3/BubbleSortMethods.java
@@ -0,0 +1,39 @@
+package w3;
+
+
+import java.util.Arrays;
+
+public class BubbleSortMethods {
+    public static void main(String[] args) {
+        String[] alphabet = {"Yankee", "November", "Alfa", "Lima", "Quebec", "Romeo", "Victor", "Whiskey", "Uniform", "Delta", "Bravo", "Mike",
+                "Foxtrot",
+                "Hotel", "X-ray", "Sierra", "Charlie", "Juliett", "Oscar", "Zulu", "Papa", "Golf", "Tango", "Echo", "India", "Kilo"};
+        System.out.println(Arrays.toString(alphabet));
+
+        alphabet = Bubbles(alphabet);
+        System.out.print("");
+        System.out.println(Arrays.toString(alphabet) + " - отсортированный массив");
+    }
+
+    public static String[] Bubbles(String[] alphabet) {
+        String tempStr;
+        for (int t = 0; t < alphabet.length - 1; t++) {
+            for (int i = 0; i < alphabet.length - t - 1; i++) {
+                if (alphabet[i + 1].compareTo(alphabet[i]) < 0) {
+                    tempStr = alphabet[i];
+                    alphabet[i] = alphabet[i + 1];
+                    alphabet[i + 1] = tempStr;
+                }
+            }
+        }
+
+        for (int i = 0; i < alphabet.length; i++) {
+
+
+
+        }return alphabet;
+    }
+
+
+}
+
