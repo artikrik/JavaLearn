41981ef91f260e2bfa31925fc2aac81caa65f57b
kpoberezhniuk <kpoberezhniuk@playtika.com>
2018-05-14 21:24:56 +0300

    FindTheLetter_fix9

--- src/w4/FindTheLetter.java
+++ src/w4/FindTheLetter.java
@@ -1,7 +1,6 @@
 
 package w4;
 
-
 import java.util.Random;
 import java.util.Scanner;
 
@@ -30,19 +29,15 @@ public class FindTheLetter {
                 System.out.println("You enter not Latin letter, please try again");
                 continue;
             }
-
-            if (letterNeedToBeFind == inputLetter) {  // Зачем это условие, если оно вынесено в for?
-                System.out.println("GREAT you have been found the hidden letter - " + inputLetter); // этот вывод - после цикла?
-
-            } else {
+            if (letterNeedToBeFind > inputLetter) {
                 System.out.println("Wrong letter try again");
-                if (letterNeedToBeFind > inputLetter) {
-                    System.out.println("You are <Too low!> to hidden letter");
-                } else if (letterNeedToBeFind < inputLetter) {
-                    System.out.println("You are <Too high!> to hidden letter");
-                }
+                System.out.println("You are <Too low!> to hidden letter");
+            } else if (letterNeedToBeFind < inputLetter) {
+                System.out.println("Wrong letter try again");
+                System.out.println("You are <Too high!> to hidden letter");
             }
         }
+        System.out.println("GREAT you have been found the hidden letter - " + inputLetter);
         System.out.println("Number of attempts = " + numberOfAttempts);
     }
 
