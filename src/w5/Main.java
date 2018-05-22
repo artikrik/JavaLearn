36e56d687149966e4669cfacbb46cc9aa13b14a2
kpoberezhniuk <kpoberezhniuk@playtika.com>
2018-05-22 17:03:50 +0300

    Wine_fix1

--- src/w5/Main.java
+++ src/w5/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Wine wine1 = new Wine("Shabodun","France",new GregorianCalendar(2000, 4, 12));
-        
+
         System.out.println(wine1.toString());
     }
 }
