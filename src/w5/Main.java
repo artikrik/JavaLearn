7993ed1c3a43cb35cf9c91d94bc98f4385879648
kpoberezhniuk <kpoberezhniuk@playtika.com>
2018-05-22 17:03:12 +0300

    Wine_fix1

--- src/w5/Main.java
+++ src/w5/Main.java
@@ -1,30 +1,13 @@
-/*
-Задача Wine.
-        Опишите класс Wine , который в дальнейшем может быть применим при создании электронного каталога вин
-        (сам электронный каталог вин реализовывать не нужно).
-        Продумайте, какие поля и методы понадобятся классу Wine, для выполнения следующих функций:
-        1. Хранение информации о виде вина: название, страна, дата разлива.
-        2. Доступ (установка и получение значений) к хранимой информации.
-        3. Расчёт выдержки вина в годах (текущая дата задаётся как параметр или определяется системной датой).
-*/
-
 package w5;
+
 
 import java.util.GregorianCalendar;
 
-import com.sun.org.apache.xpath.internal.operations.String;
-
 public class Main {
-
     public static void main(String[] args) {
 
-        Wine wine1 = new Wine();
-        wine1.setName ("Swabo");
-
-        Wine wine2 = new Wine();
-
-        Wine wine3 = new Wine();
-
-        System.out.println(wine1);
+        Wine wine1 = new Wine("Shabodun","France",new GregorianCalendar(2000, 4, 12));
+        
+        System.out.println(wine1.toString());
     }
 }
--- src/w5/Wine.java
+++ src/w5/Wine.java
@@ -1,14 +1,43 @@
 package w5;
 
 
+import java.util.Calendar;
 import java.util.GregorianCalendar;
+
+import static java.util.Calendar.*;
 
 
 public class Wine {
     private String name;
+    private String country;
+    private GregorianCalendar dateOfBottling;
 
-    public void setName (String name){
+    public Wine(String name, String country, GregorianCalendar dateOfBottling) {
+        setName(name);
+        setCountry(country);
+        setDateOfBottling(dateOfBottling);
+    }
+
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setDateOfBottling(GregorianCalendar dateOfBottling) {
+        this.dateOfBottling = dateOfBottling;
+    }
+
+    public int wineExtract() {
+        return dateOfBottling.get(Calendar.DATE) - new GregorianCalendar().get(DATE);
+    }
+
+
+    public String toString() {
+        return "Name - " + name + "\nCountry -  " + country + "\nWine extract " + wineExtract() + " years";
     }
 
 
