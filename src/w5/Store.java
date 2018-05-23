5d28831c026f15aa6a381450dee6d48a13d4869c
aalokhin <12104@ukr.net>
2018-05-23 14:11:55 +0300

    Fix

--- src/w5/Store.java
+++ src/w5/Store.java
@@ -1,14 +1,11 @@
 package w5;
 
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+
 public class Store {
     public static void main(String[] args) {
-
-
-        Wine wine = new Wine("NoName", "NoWhere", new GregorianCalendar(2000, 5, 23));
-
-
-
+        LocalDate date = LocalDate.of(2000, 5, 23);
+        Wine wine = new Wine("NoName", "NoWhere", date);
         System.out.println(wine.toString());
 
     }
