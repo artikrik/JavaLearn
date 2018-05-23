b2255ee010c5d4eff904db41c04f5a8471f5d42d
aalokhin <12104@ukr.net>
2018-05-23 17:44:18 +0300

    Task number one is done.

---
+++ src/w2/Task1.java
@@ -0,0 +1,21 @@
+package w2;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        int[] array;
+        array = new int[50];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i * 2 + 1;
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println("");
+        for (int i = array.length - 1; i >= 0; i--) {
+            array[i] = i * 2 + 1;
+            System.out.print(array[i] + " ");
+        }
+    }
+}
+
+
