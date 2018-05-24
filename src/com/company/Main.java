56c098b18c275feab4b89a2a301492e5bd956257
iliashuk <iliashuk@playtika.com>
2018-05-24 18:12:08 +0300

    Added Car class implementation.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import java.time.LocalDate;
 import java.time.Month;
 
+import com.company.week5.Car;
 import com.company.week5.Wine;
 
 public class Main {
@@ -12,6 +13,11 @@ public class Main {
         Wine wine2 = new Wine("Yellow Tail", "Australia", LocalDate.of(2013, Month.APRIL, 13));
         System.out.println(wine1.toString() + "\n" + wine2.toString());
         System.out.println("Ageing of wine1 = " + wine1.calculateAgeingOfWine() + " years");
-    }
 
+        Car car1 = new Car(2010, "Mazda");
+        Car car2 = new Car(2015, "Audi");
+
+        car1.showResultOfChangeSpeedFewTimes(5, "+");
+        car1.showResultOfChangeSpeedFewTimes(5, "-");
+    }
 }
