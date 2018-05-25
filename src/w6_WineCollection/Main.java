11a72d2609f6adecba19a4d396d5c5aa4fb1b8a2
kpoberezhniuk <kpoberezhniuk@playtika.com>
2018-05-25 23:05:01 +0300

    WineCollection_fix0

--- src/w6_WineCollection/Main.java
+++ src/w6_WineCollection/Main.java
@@ -3,16 +3,38 @@ package w6_WineCollection;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
+
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<WineCollection> wineCollection = new ArrayList<>();
-        wineCollection.add(new WineCollection("Shabodun", "France", new GregorianCalendar(1800, 4, 12)));
-        wineCollection.add(new WineCollection("Kagore", "Krim", new GregorianCalendar(2010, 1, 27)));
-        wineCollection.add(new WineCollection("Marami", "Hawaiy", new GregorianCalendar(2000, 11, 4)));
-        wineCollection.add(new WineCollection("Kagore", "Krim", new GregorianCalendar(1970, 5, 13)));
+        ArrayList<WineCollection> arrayList = new ArrayList<WineCollection>();
+        arrayList.add(new WineCollection("Shabodun", "France", new GregorianCalendar(1800, 5, 13)));
+        arrayList.add(new WineCollection("Kagore", "Krim", new GregorianCalendar(2010, 1, 27)));
+        arrayList.add(new WineCollection("Marami", "Hawaiy", new GregorianCalendar(1970, 11, 4)));
+        arrayList.add(new WineCollection("Kagore", "Krim", new GregorianCalendar(2000, 5, 13)));
+        arrayList.add(new WineCollection("Kagorenei", "Krimatoriy", new GregorianCalendar(2010, 5, 13)));
 
-        System.out.println(wineCollection);
+        arrayList.sort(WineCollection.Extracting);
 
+        int minExtracting = arrayList.get(0).wineExtract();
+
+        for (WineCollection i : arrayList) {
+            if (minExtracting >= i.wineExtract()) {
+                minExtracting = i.wineExtract();
+            }
+        }
+        youngestWine(arrayList, minExtracting);
+
+        System.out.println(youngestWine(arrayList,minExtracting));
+    }
+
+    public static ArrayList<WineCollection> youngestWine(ArrayList<WineCollection> arrayList, int minExtracting) {
+        ArrayList<WineCollection> youngestWine = new ArrayList<>();
+        for (WineCollection w : arrayList) {
+            if (minExtracting == w.wineExtract()) {
+                youngestWine.add(w);
+            }
+        }
+        return youngestWine;
     }
 }
--- src/w6_WineCollection/WineCollection.java
+++ src/w6_WineCollection/WineCollection.java
@@ -1,17 +1,19 @@
 package w6_WineCollection;
 
+import java.util.Comparator;
 import java.util.GregorianCalendar;
 
-public class WineCollection {
+public class WineCollection{
     private String name;
     private String country;
     private GregorianCalendar dateOfBottling;
+
 
     public WineCollection(String name, String country, GregorianCalendar dateOfBottling) {
         setName(name);
         setCountry(country);
         setDateOfBottling(dateOfBottling);
-    }
+        }
 
     public void setName(String name) {
         this.name = name;
@@ -41,12 +43,20 @@ public class WineCollection {
         return new GregorianCalendar().get(GregorianCalendar.YEAR) - dateOfBottling.get(GregorianCalendar.YEAR);
     }
 
-    public String toString() {
-        return "\nName - " + name + "\nCountry -  " + country + "\nWine extract " + wineExtract() + " years" + "\n";
+    public int getWineExtract() {
+        return wineExtract();
     }
 
+    public static Comparator<WineCollection> Extracting = new Comparator<WineCollection>() {
 
+        public int compare(WineCollection w1, WineCollection w2) {
+            int wineExtract0 = w1.getWineExtract();
+            int wineExtract1 = w2.getWineExtract();
+            return wineExtract0 - wineExtract1;
+        }
+    };
+
+    public String toString() {
+        return "\nName - " + name + "\nCountry -  " + country + "\nWine extract " + wineExtract() + " years " + "\n";
+    }
 }
-
-
-
