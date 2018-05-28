0ee158c2be53e8d953ab5b8a7e79be5e5b9e47ab
oginda <oginda@playtika.com>
2018-05-28 12:00:43 +0300

    Wines

--- src/com/company/Wine/Main.java
+++ src/com/company/Wine/Main.java
@@ -1,4 +1,14 @@
 package com.company.Wine;
 
+import java.util.GregorianCalendar;
+
 public class Main {
+    public static void main(String[] args) {
+        WineCatalog wines = new WineCatalog("Old Gruzia",
+                                            "Gruzia",
+                                            new GregorianCalendar(1973, 05,10));
+        System.out.println("Вино " + wines.getWineName() +
+                            " производства " + wines.getWineCountry() +
+                            " с выдержкой " + wines.getBottlingYear() + " лет.");
+    }
 }
---
+++ src/com/company/Wine/WineCatalog.java
@@ -0,0 +1,48 @@
+package com.company.Wine;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class WineCatalog {
+    private String wineName;
+    private String wineCountry;
+    private GregorianCalendar bottlingYear;
+
+    public WineCatalog(String wineName, String wineCountry,
+                       GregorianCalendar bottlingYear){
+        setWineName(wineName);
+        setWineCountry(wineCountry);
+        setBottlingYear(bottlingYear);
+    }
+
+
+    public void setWineName(String wineName) {
+        this.wineName = wineName;
+    }
+
+    public void setWineCountry(String wineCountry) {
+        this.wineCountry = wineCountry;
+    }
+
+    public void setBottlingYear(GregorianCalendar bottlingYear) {
+        this.bottlingYear = bottlingYear;
+    }
+
+    public String getWineName() {
+        return wineName;
+    }
+
+    public String getWineCountry() {
+        return wineCountry;
+    }
+
+    public int getBottlingYear() {
+        return new GregorianCalendar().get(Calendar.YEAR) - bottlingYear.get(Calendar.YEAR);
+    }
+
+    @Override
+    public String toString() {
+        return "Вино " + wineName + " произведено в " + wineCountry +
+                " c выдержкой " + bottlingYear + ".";
+    }
+}
