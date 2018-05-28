e4d495eef576d9e22c5998ce2ba35951d49ea452
oginda <oginda@playtika.com>
2018-05-28 13:08:02 +0300

    overload methods

---
+++ src/com/company/ForthTask/BubbleOverloadingMethods.java
@@ -0,0 +1,49 @@
+package com.company.ForthTask;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class BubbleOverloadingMethods {
+    public static void main(String[] args) {
+        String[] phones = {"iPhone", "Samsung", "Nokia", "Xiaomi", "Meizu",
+                "LG", "Huawei", "Asus", "Sony", "Motorola", "HTC", "1+"};
+        System.out.println(Arrays.toString(phones));
+        bubleSort(phones);
+        System.out.println(Arrays.toString(phones));
+
+        int[] mass = new int[15];
+        Random random = new Random();
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = random.nextInt(50);
+        }
+        System.out.println(Arrays.toString(mass));
+        bubleSort(mass);
+        System.out.println(Arrays.toString(mass));
+    }
+
+    static int[] bubleSort(int[] mass) {
+        for (int i = mass.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (mass[j] > mass[j + 1]) {
+                    int tmp = mass[j];
+                    mass[j] = mass[j + 1];
+                    mass[j + 1] = tmp;
+                }
+            }
+        }
+        return mass;
+    }
+
+    static String[] bubleSort(String[] mass) {
+        for (int i = mass.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (mass[j].compareTo(mass[j + 1]) > 0) {
+                    String tmp = mass[j];
+                    mass[j] = mass[j + 1];
+                    mass[j + 1] = tmp;
+                }
+            }
+        }
+        return mass;
+    }
+}
