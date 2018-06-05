4d2b2ed922dab5970397cd5d377a19c9c9465eb4
omakarchuk <alexandermakarchuk2@gmail.com>
2018-06-05 15:29:27 +0300

    Done MarathonComparable&Comparator.

---
+++ src/com/company/W6/MarathonComparableAndComparator/MarathonComparableAndComparator.java
@@ -0,0 +1,30 @@
+package com.company.W6.MarathonComparableAndComparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class MarathonComparableAndComparator {
+    public static void main(String[] args) {
+
+        Sportsmen sportsmen1 = new Sportsmen("Vitya", 300);
+        Sportsmen sportsmen2 = new Sportsmen("Anya", 322);
+        Sportsmen sportsmen3 = new Sportsmen("Anya", 222);
+        Sportsmen sportsmen4 = new Sportsmen("Kostya", 111);
+
+        ArrayList<Sportsmen> sportsmenList = new ArrayList<>();
+        sportsmenList.add(sportsmen1);
+        sportsmenList.add(sportsmen2);
+        sportsmenList.add(sportsmen3);
+        sportsmenList.add(sportsmen4);
+
+        Collections.sort(sportsmenList);
+        for (Sportsmen sportsmen: sportsmenList) {
+            System.out.print(sportsmen.toString());
+        }
+
+        Collections.sort(sportsmenList,Comparator.comparing(Sportsmen::getSeconds));
+        System.out.println("1 place -  " + sportsmenList.get(0) + "2 place -  " + sportsmenList.get(1) + "3 place -  " + sportsmenList.get(2));
+
+    }
+}
---
+++ src/com/company/W6/MarathonComparableAndComparator/Sportsmen.java
@@ -0,0 +1,45 @@
+package com.company.W6.MarathonComparableAndComparator;
+
+public class Sportsmen implements Comparable<Sportsmen> {
+    private String name;
+    private int seconds;
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public Sportsmen(String name, int seconds) {
+        setName(name);
+        setSeconds(seconds);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public String toString() {
+        return name +"\n" ;
+    }
+
+    @Override
+    public int compareTo(Sportsmen o) {
+        if (this.name.compareTo(o.getName()) != 0)
+            return this.name.compareTo(o.getName());
+        else
+            return this.seconds - o.getSeconds();
+    }
+}
