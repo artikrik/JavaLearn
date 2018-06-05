d9a260a2e660e08c5051f240e335fd978554b7a0
kpoberezhniuk <kpoberezhniuk@playtika.com>
2018-06-05 20:25:31 +0300

    MarathoneCompAndCompar

---
+++ src/w6_MarathonComparableAndComparator/Main.java
@@ -0,0 +1,33 @@
+/*Программа должна сначала вывести список всех участников забега,
+        отсортированный по именам (в лексикографическом порядке),
+        а если имена одинаковые, тогда по времени бега от меньшего к большему.
+        После этого программа должна вывести список трех призеров.*/
+
+package w6_MarathonComparableAndComparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<MarathoneCompAndCompar> arrayList = new ArrayList<>();
+        arrayList.add(new MarathoneCompAndCompar("Begunovenko", 15));
+        arrayList.add(new MarathoneCompAndCompar("Petrov", 3));
+        arrayList.add(new MarathoneCompAndCompar("Begunova", 5));
+        arrayList.add(new MarathoneCompAndCompar("Morgunov", 40));
+        arrayList.add(new MarathoneCompAndCompar("Begunova", 13));
+        arrayList.add(new MarathoneCompAndCompar("Plavunov", 47));
+
+        Collections.sort(arrayList, new Comparator<MarathoneCompAndCompar>() {
+            @Override
+            public int compare(MarathoneCompAndCompar m1, MarathoneCompAndCompar m2) {
+                return m1.getTime() - m2.getTime();
+            }
+        });
+
+        for (MarathoneCompAndCompar marathone : arrayList) ;
+        System.out.println(arrayList.get(0).toString() + "\n" + arrayList.get(1).toString() + "\n" + arrayList.get(2).toString());
+    }
+}
---
+++ src/w6_MarathonComparableAndComparator/MarathoneCompAndCompar.java
@@ -0,0 +1,39 @@
+package w6_MarathonComparableAndComparator;
+
+
+public class MarathoneCompAndCompar implements Comparable<MarathoneCompAndCompar> {
+    private String name;
+    private int time;
+
+    public MarathoneCompAndCompar(String name, int time) {
+        setName(name);
+        setTime(time);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public String toString() {
+        return "\nName - " + name + "\nTime " + time + "\n";
+    }
+
+    @Override
+    public int compareTo(MarathoneCompAndCompar m) {
+        if (this.name.compareTo(m.getName()) != 0)
+            return this.name.compareTo(m.getName());
+        else return this.time - m.getTime();
+    }
+}
