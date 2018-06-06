379f7ea7d15ed45d19358a1fb7bed9270a9f716d
aalokhin <12104@ukr.net>
2018-06-06 10:07:53 +0300

    Wine with comparable update

--- src/w6Comparable/WineStore.java
+++ src/w6Comparable/WineStore.java
@@ -10,10 +10,10 @@ public class WineStore {
     public static void main(String[] args) {
         List<Wine> wineCatalog = new ArrayList<>();
         wineCatalog.add(new Wine("NoName1", "NoWhere1", 14));
-        wineCatalog.add(new Wine("NoName1", "NoWhere1", 13));
-        wineCatalog.add(new Wine("NoName1", "NoWhere1", 12));
-        wineCatalog.add(new Wine("NoName1", "NoWhere1", 9));
-        wineCatalog.add(new Wine("NoName1", "NoWhere1", 10));
+        wineCatalog.add(new Wine("NoName2", "NoWhere2", 13));
+        wineCatalog.add(new Wine("NoName3", "NoWhere3", 12));
+        wineCatalog.add(new Wine("NoName4", "NoWhere4", 9));
+        wineCatalog.add(new Wine("NoName5", "NoWhere5", 10));
         Wine youngestWine = Collections.min(wineCatalog);
 
         for (Wine wine : wineCatalog) {
