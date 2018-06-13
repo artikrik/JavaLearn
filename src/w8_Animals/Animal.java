9fac3c963125af5e9b0680936f0ef37c837916b6
kpoberezhniuk <kpoberezhniuk@playtika.com>
2018-06-13 14:20:24 +0300

    w8_Animals_fix5

--- src/w8_Animals/Animal.java
+++ src/w8_Animals/Animal.java
@@ -1,9 +1,6 @@
 package w8_Animals;
 public class Animal {
 
-    private String eat;
-    private String sleep;
-
     public Animal() {
         System.out.println("A new animal has been created!");
     }
