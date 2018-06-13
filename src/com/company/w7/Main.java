3f078d4043f93caf77a816b477e872ba103f3987
odolgyi <veterokvn.ua@gmail.com>
2018-06-13 08:21:35 +0300

    W7. Fix

--- src/com/company/w7/Main.java
+++ src/com/company/w7/Main.java
@@ -1,12 +1,22 @@
 package com.company.w7;
 
-import static com.company.w7.MovablePointsSet.numberOfMove;
+import static com.company.w7.MovablePointsSet.setRandomMove;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Создание объектов");
-        MovablePointsSet movablePointsSet = new MovablePointsSet();
+        MovablePointsSet movablePointsSet = new MovablePointsSet(26,10,11,0);
         movablePointsSet.toString();
-        numberOfMove(movablePointsSet, 5); // метод для объект класса MovablePointsSet, который перемещает все точки 5 раз
+        numberOfMove(movablePointsSet, 5, 7,-3); // метод для объект класса MovablePointsSet, который перемещает все точки 5 раз
+    }
+
+    public static void numberOfMove(MovablePointsSet movablePointsSet, int numberOfMove, int maxValueOfMove, int minValueOfMove) {
+        for (int i = 0; i < numberOfMove; i++) {
+            System.out.println("Перемещаем каждую из точек " + (i + 1) + " раз");
+            for (MovablePoint valueSpeed : movablePointsSet.points) {
+                valueSpeed.move(setRandomMove(maxValueOfMove, minValueOfMove), setRandomMove(maxValueOfMove, minValueOfMove));
+            }
+            movablePointsSet.toString();
+        }
     }
 }
--- src/com/company/w7/MovablePointsSet.java
+++ src/com/company/w7/MovablePointsSet.java
@@ -5,19 +5,15 @@ import java.util.Random;
 
 public class MovablePointsSet {
 
-    final int minCoordinate = 10;
-    final int maxCoordinate = 26;
-    final int minSpeed = 0;
-    final int maxSpeed = 11;
     ArrayList<MovablePoint> points = new ArrayList<>();
 
-    public MovablePointsSet() {
+    public MovablePointsSet(int maxCoordinate, int minCoordinate,int maxSpeed, int minSpeed) {
         for (int i = 0; i < 5; i++) {
             points.add(setRandomValue(maxCoordinate, minCoordinate, maxSpeed, minSpeed));
         }
     }
 
-    public MovablePointsSet(int numberOfPoint) {
+    public MovablePointsSet(int numberOfPoint, int maxCoordinate, int minCoordinate,int maxSpeed, int minSpeed) {
         for (int i = 0; i < numberOfPoint; i++) {
             points.add(setRandomValue(maxCoordinate, minCoordinate, maxSpeed, minSpeed));
         }
@@ -33,16 +29,6 @@ public class MovablePointsSet {
                 new Random().nextInt(maxCoordinate) + minCoordinate,
                 new Random().nextInt(xSpeedRandom) + ySpeedRandom,
                 new Random().nextInt(xSpeedRandom) + ySpeedRandom);
-    }
-
-    public static void numberOfMove(MovablePointsSet movablePointsSet, int number) {
-        for (int i = 0; i < number; i++) {
-            System.out.println("Перемещаем каждую из точек " + (i + 1) + " раз");
-            for (MovablePoint valueSpeed : movablePointsSet.points) {
-                valueSpeed.move(setRandomMove(7, -3), setRandomMove(7, -3));
-            }
-            movablePointsSet.toString();
-        }
     }
 
     @Override
