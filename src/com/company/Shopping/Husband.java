a4ceca041689319901ddde29bb53dca3c4e49b52
oginda <oginda@playtika.com>
2018-06-26 13:38:37 +0300

    Shop, in progress

--- src/com/company/Shopping/Husband.java
+++ src/com/company/Shopping/Husband.java
@@ -1,10 +1,23 @@
 package com.company.Shopping;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Husband {
 
     ArrayList<PurchasingCharacteristics> allItemsInAllShops = new ArrayList<>();
-    ArrayList<PurchasingCharacteristics>
+    ArrayList<PurchasingCharacteristics> allowableProducts = new ArrayList<>();
+    Silpo silpo = new Silpo();
+    Comfy comfy = new Comfy();
+
+    public void silpoInfo(Silpo products) {
+        silpo = products;
+    }
+
+    public void comfyInfo(Comfy gadgets) {
+        comfy = gadgets;
+    }
+
+    public List<PurchasingCharacteristics>
 
 }
--- src/com/company/Shopping/Main.java
+++ src/com/company/Shopping/Main.java
@@ -1,11 +1,25 @@
 package com.company.Shopping;
 
+import com.company.WineCollection.Wine;
+
 public class Main {
     public static void main(String[] args) {
 
         PurchasingCharacteristics purchasingCharacteristics = new PurchasingCharacteristics();
-        Megamoll megamoll = new Megamoll();
         Silpo silpo = new Silpo();
+        Comfy comfy = new Comfy();
+
+
+
+        Husband husband = new Husband();
+        husband.silpoInfo(silpo);
+        husband.comfyInfo(comfy);
+
+        Wife wife = new Wife();
+        wife.getAllItemsListFromHusband();
+        wife.getNeedToBuyItems();
+        wife.getTotalCost();
+
 
     }
 }
