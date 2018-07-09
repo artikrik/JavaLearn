4535072bc3b679e68cb5fea0a7edcf5b7db54d42
vlisovyi <Amalker_1983>
2018-07-09 23:42:50 +0300

    Countries.

---
+++ src/com/company/w10/Countries.java
@@ -0,0 +1,26 @@
+package w10;
+
+import java.util.Map;
+
+public enum Countries {
+
+    Ukraine("Ukraine", 42000000), Russia("Russia", 160000000), America("America", 250000000);
+
+    private String name;
+    private int population;
+
+    Map<String, Integer> country;
+
+    Countries(String name, int population) {
+        this.population = population;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+}
---
+++ src/com/company/w10/MainMap.java
@@ -0,0 +1,23 @@
+package w10;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainMap {
+
+
+    public static void main(String[] args) {
+        Countries russia = Countries.Russia;
+        Countries america = Countries.America;
+        Countries ukraine = Countries.Ukraine;
+
+        Map<String, Integer> country = new HashMap<>();
+        country.put(ukraine.getName(),ukraine.getPopulation());
+        country.put(russia.getName(),russia.getPopulation());
+        country.put(america.getName(),america.getPopulation());
+
+        System.out.println(country.toString());
+
+        System.out.println("Population of " + ukraine.getName() +  " = " + country.get(ukraine.getName()));
+    }
+}
