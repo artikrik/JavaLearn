f178acad59a5cd0ba8bc1a236fb0eaeb11213159
kpoberezhniuk <kpoberezhniuk@playtika.com>
2018-07-11 18:50:55 +0300

    w9_Countries_fix3

--- src/w9_Countries/Countries.java
+++ src/w9_Countries/Countries.java
@@ -1,16 +1,6 @@
 package w9_Countries;
 
-public enum Countries {
+public enum  Countries {
 
-    UGANDA(100000), USA(500000), UKRAINE(400000), TURKEY(3000);
-
-    private long numberOfPopulation;
-
-    Countries(long numberOfPopulation) {
-        this.numberOfPopulation = numberOfPopulation;
-    }
-
-    public long getNumberOfPopulation() {
-        return numberOfPopulation;
-    }
+    UGANDA, USA, UKRAINE, TURKEY;
 }
--- src/w9_Countries/Main.java
+++ src/w9_Countries/Main.java
@@ -6,23 +6,19 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        Map<String,Integer> map = new HashMap<>();
-        map.put("UGANDA", 100000);
-        map.put("USA", 500000);
-        map.put("UKRAINE", 400000);
-        map.put("TURKEY", 3000);
+        Map<Countries,Integer> map = new HashMap<>();
+        map.put(Countries.UGANDA,100000);
+        map.put(Countries.USA, 500000);
+        map.put(Countries.UKRAINE, 400000);
+        map.put(Countries.TURKEY, 3000);
 
         for (Map.Entry map2: map.entrySet()) {
             System.out.println(map2.getKey()+" = "+map2.getValue()); //show all countries and population
         }
-        Countries Uganda = Countries.UGANDA;
-        Countries USA = Countries.USA;
-        Countries Ukarine = Countries.UKRAINE;
-        Countries Turkey = Countries.TURKEY;
 
-        System.out.println(Countries.UGANDA.getNumberOfPopulation()); // show information about countries
-        System.out.println(Countries.USA.getNumberOfPopulation()); // show information about countries
-        System.out.println(Countries.UKRAINE.getNumberOfPopulation()); // show information about countries
-        System.out.println(Countries.TURKEY.getNumberOfPopulation()); // show information about countries
+        System.out.println("\n"+map.get(Countries.UGANDA)); // show information about countries
+        System.out.println(map.get(Countries.USA)); // show information about countries
+        System.out.println(map.get(Countries.UKRAINE)); // show information about countries
+        System.out.println(map.get(Countries.TURKEY)); // show information about countries
     }
 }
