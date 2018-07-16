9bc14e4356394f0106db9cc310cdfa8fbe9087e9
kpoberezhniuk <kpoberezhniuk@playtika.com>
2018-07-16 15:53:33 +0300

    w9_Countries_fix5

--- src/w9_Aircraft/Aircraft.java
+++ src/w9_Aircraft/Aircraft.java
@@ -4,19 +4,14 @@ enum Aircraft {
 
     Airbus, Boing, Embraer;
 
+
+
+
+
     public Model model;
 
     enum Model {
         A310, A320, A330, B777, B737, B747, B767, E175, E190, E195;
     }
 
-
-
-    /*private String producer;
-    private String model;
-
-    Aircraft(String producer, String model) {
-        this.producer = producer;
-        this.model = model;
-    }*/
 }
