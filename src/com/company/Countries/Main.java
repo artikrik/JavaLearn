b3c585ec691521ca716bccf2b50b5e560f726284
oginda <oginda@playtika.com>
2018-07-24 11:37:47 +0300

    aircraft done

--- src/com/company/Countries/Main.java
+++ src/com/company/Countries/Main.java
@@ -5,17 +5,17 @@ public class Main {
     public static void main(String[] args) {
         CountriesMap map = new CountriesMap();
 
-        map.addCountry(new Countries("Ukraine", (long) 45000000));
-        map.addCountry(new Countries("Moldova", (long) 3552000));
-        map.addCountry(new Countries("Germany", (long) 82670000));
-        map.addCountry(new Countries("Russia", (long) 144300000));
+        map.addCountry(new Countries("Ukraine", 45000000l));
+        map.addCountry(new Countries("Moldova", 3552000l));
+        map.addCountry(new Countries("Germany", 82670000l));
+        map.addCountry(new Countries("Russia", 144300000l));
 
         map.print();
 
-        System.out.println(map.search("Ukraine"));
-        System.out.println(map.search("Moldova"));
-        System.out.println(map.search("Germany"));
-        System.out.println(map.search("Russia"));
+        System.out.println("популяция Украины: " + map.search("Ukraine"));
+        System.out.println("популяция Украины: " + map.search("Moldova"));
+        System.out.println("популяция Украины: " + map.search("Germany"));
+        System.out.println("популяция Украины: " + map.search("Russia"));
     }
 
 }
